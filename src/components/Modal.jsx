08bf18ce4983b99d320e9a59adc4e7c5b159bc86
Emmy3131 <devinechild30032@gmail.com>
2025-12-24 22:55:56 +0000

    wip

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ const Modal = ({ isOpen, onClose, title, children }) => {
     isOpen ? (
       <div id="addProductContainer" className=" w-full h-screen m-auto fixed top-0 left-0 bg-black/50 bg-opacity-50 z-20">
 
-        <div className="w-full h-screen m-auto flex items-center justify-center">
+        <div className="w-full h-screen m-auto flex items-center justify-center overflow-x-auto">
           <div className="bg-gradient-to-br from-green-800 to-emerald-600 p-8 rounded-2xl shadow w-[700px]">
             <div className="bg-white rounded-2xl shadow-xl p-6 w-full">
              {children}
